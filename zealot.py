d3e7f7771897576cf43eec5673eb27e13584ef61
Tom Jorquera <tom.jorquera@linagora.com>
2017-01-04 14:36:23 +0100

    add git support

--- zealot.py
+++ zealot.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-import os, shutil, logging
+import sys, os, shutil, logging, git, re
 from datetime import date
-from sacred import Experiment, dependencies
+from sacred import Experiment, dependencies, arg_parser
 from zealot_ingredient_env import env
 from zealot_ingredient_env_basic import setup_env_basic
 from zealot_ingredient_env_docker import setup_env_docker
@@ -22,10 +22,16 @@ from zealot_ingredient_env_docker import setup_env_docker
 
 zealot = Experiment('Zealot', ingredients=[env])
 
+##
+# Global config
+_git_storage = os.path.join(os.path.expanduser('~'), '.zealot_git_storage')
+
 @zealot.config
 def zealot_config():
-    storage_folder = os.path.join(os.path.expanduser("~"), "Experiments")
+    storage_folder = os.path.join(os.path.expanduser('~'), 'Experiments')
     log_level = 'INFO'
+    git_url = None
+    git_storage = _git_storage
 
 def save_artifacts(zealot, base_path):
     for artifact in os.listdir(base_path):
@@ -82,7 +88,33 @@ def main(env, _log):
     _log.info('cleaning up')
     running_env.close()
 
+def clone_or_udpdate_git_repo(git_url, git_storage):
+        project_name = re.search('.*/(.+?)\.git$', git_url).group(1)
+
+        git_loc = os.path.join(git_storage, project_name)
+
+        # clone repo if required
+        if not os.path.exists(git_loc):
+            git.Repo.clone_from(git_url,
+                                git_loc,
+                                depth=1)
+
+        repo = git.Repo(git_loc)
+        # update repo to latest version
+        repo.remotes.origin.pull('master')
+        return git_loc
+
 if __name__ == "__main__":
+
+    args = arg_parser.parse_args(sys.argv)
+    conf_updates = arg_parser.get_config_updates(args['UPDATE'])
+
+    if('git_url' in conf_updates[0]):
+        # checkout/update repo, add ref to config, and go into working dir
+        git_url = conf_updates[0]['git_url']
+        git_loc = clone_or_udpdate_git_repo(git_url, _git_storage)
+        zealot.add_config(git_rev = str(git.Repo(os.getcwd()).head.commit))
+        os.chdir(git_loc)
 
     # add step files as sources
     for step in [line.rstrip('\n') for line in open(os.path.join(os.getcwd(), 'steps.txt'))]:
