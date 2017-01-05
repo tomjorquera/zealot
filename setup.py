e051920ad2770f4c205ba047d487333fd6ba6a32
Tom Jorquera <tom.jorquera@linagora.com>
2017-01-05 17:08:05 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+from setuptools import setup, find_packages
+
+setup(
+    name='zealot',
+    version='0.0.1',
+    author='Tom Jorquera',
+    author_email='tjorquera@linagora.com',
+    packages = find_packages(),
+    classifiers=[
+       'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)'
+    ],
+    entry_points = {
+        'console_scripts' : ['zealot=zealot.main:zealot_main']
+    },
+    install_requires = [
+        'sacred',
+        'gitpython',
+        'docker'
+    ],
+)
